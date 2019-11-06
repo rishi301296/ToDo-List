cc6ddf880b51524b1d80f3a465cf77ff89ef9107
rishi301296 <rishi.bose@kuliza.com>
2019-11-07 03:52:56 +0530

    admin.py change

--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from api.models import Task, User
 
 # Register your models here.
-admin.register(Task)
-admin.register(User)
+admin.site.register(Task)
+admin.site.register(User)
